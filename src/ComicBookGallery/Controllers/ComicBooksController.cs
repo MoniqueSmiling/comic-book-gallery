327684c27b318c56c266ab7ba53328f5cabe285a
conta <conta@DESKTOP-26VQ22K>
2016-08-13 20:08:02 +0200

    Updated Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,18 @@ namespace ComicBookGallery.Controllers // need to be public, or MVC can't find a
     {
         public ActionResult Detail() // everything inside of controller is an action
         {
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man"; //property
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
             return View();
         }
     }
