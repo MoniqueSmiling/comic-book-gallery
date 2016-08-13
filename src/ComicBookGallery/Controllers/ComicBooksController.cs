cb4e0b28dcb1b75bca939114920801ff3103fa5b
conta <conta@DESKTOP-26VQ22K>
2016-08-13 19:21:12 +0200

    Updated detail action method to use action result types

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ComicBookGallery.Controllers
+namespace ComicBookGallery.Controllers // need to be public, or MVC can't find and call it
 {
-    public class ComicBooksController : Controller // need to be public, or MVC can't find or call it
+    public class ComicBooksController : Controller // need to be public, or MVC can't find and call it
     {
-        public string Detail()
+        public ActionResult Detail() // everything inside of controller is an action
         {
-            return "Hello from the comic book controller";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Hello from the comic book controller!");
         }
     }
 }
