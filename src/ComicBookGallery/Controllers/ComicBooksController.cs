830763fed831174aee4944d4aa1c67a308bb5501
conta <conta@DESKTOP-26VQ22K>
2016-08-13 16:39:11 +0200

    Added detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,8 +6,11 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller
+    public class ComicBooksController : Controller // need to be public, or MVC can't find or call it
     {
-
+        public string Detail()
+        {
+            return "Hello from the comic book controller";
+        }
     }
 }
